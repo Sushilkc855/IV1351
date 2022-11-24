9b320a59cd79b09030f472d24b6ab1dadf6113c8
KCchronos <sushilkc855@outlook.com>
2022-11-24 22:32:49 +0100

    Add files via upload

---
+++ fakedata.sql
@@ -0,0 +1,37 @@
+INSERT INTO person (first_name,street,person_number_UNIQUE,city,zip_code,last_name)
+VALUES
+  ('Oliver','Ap #455-6882 Ut Road','1234567891','Lutsel K''e','79095','Andersson'),
+    ('John','Ap #455-6882 Ut Road','45435435345','Lutsel K''e','79095','gg');
+
+INSERT INTO student (skill_level,student_identification_UNIQUE,number_of_lesson_taken, instrument )
+VALUES
+  ('beginner','0123345678','6','drums'),
+  ('Advanced','343343','8','guitar');
+
+INSERT INTO instructor (instructor_id_unique,number_of_group_lessions,number_of_individual_lessions,instrument)
+VALUES
+  ('43','3','45','car');
+
+INSERT INTO prices (price_biginner_intermediate,price_advanced,price_individual,price_group_lessons)
+VALUES
+  ('100','200','500','80');
+
+INSERT INTO discount (discount_percentage)
+VALUES
+  ('10.4'),
+  ('50.00');
+
+INSERT INTO phone_number (phone_no_UNIQUE)
+VALUES
+  ('789421904'),
+  ('7649421904');
+
+INSERT INTO email_adress (email_adress_UNIQUE)
+VALUES
+  ('FJHASKFDAS@GMAIL.COM'),
+('fgfdgdfFDAS@GMAIL.COM');
+
+INSERT INTO siblings (student_id_UNIQUE, relation,number_of_siblings)
+VALUES
+  ('9421412','brother','1'),
+   ('94551412','sister brother','2');
